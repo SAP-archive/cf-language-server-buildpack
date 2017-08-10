3414644038947e3706bf375bed98fbc2062a830a
PavelSosin <pavel.sosin@sap.com>
2017-08-10 12:02:52 +0300

    Multiproj (#4)

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-#declare -i STDIN_PORT=8991; export STDIN_PORT
-#declare -i STDOUT_PORT=8990; export STDOUT_PORT
-#
-#kill_lsp () {
-#    pkill -P $BASHPID
-#}
-#trap kill_lsp TERM
-exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root
+
+if [ -n "$2" ]; then
+if [ -n "$3" ]; then
+    moduleWs="$2/$3"
+else 
+    moduleWs="$2"
+fi
+
+if [ -d "$HOME/jdt_ws_root/$moduleWs"  ] && [ -n "$moduleWs" ]; then
+    echo "Already exists $moduleWs"
+else
+    echo "Create $HOME/jdt_ws_root/$moduleWs"
+    mkdir $HOME/jdt_ws_root/$moduleWs
+fi
+    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root/$moduleWs
+else
+    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root
+fi
