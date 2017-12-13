f5ab30b618905ac54921fb770306847cfc94b837
Christoph Pohl <christoph.pohl@sap.com>
2017-12-13 17:10:09 +0100

    Delete link_logs.sh

--- resources/language_server_node_cdx/link_logs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm ~/logs/cdx.log
-echo '' >~/app/.java-buildpack/language_server_node_cdx/cdx_lsp_server.log
-ln -s ~/app/.java-buildpack/language_server_node_cdx/cdx_lsp_server.log ~/logs/cdx.log
