b733be89597b5fe4b1f19c081bea50a42b126dff
Christoph Pohl <christoph.pohl@sap.com>
2017-12-13 16:26:20 +0100

    Update launcher.sh

--- resources/language_server_node_cdx/launcher.sh
+++ resources/language_server_node_cdx/launcher.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-exec npm start
+echo 'Starting server.js using node...' >launcher.log
+./link_logs.sh &>/dev/null </dev/null &
+exec ./nodejs/bin/node ./src/server.js --stdio --strict-code-completion
