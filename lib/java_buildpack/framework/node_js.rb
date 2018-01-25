1802e6b3a85e70d075527226134b477ca6c9049d
Shahar Harari <shahar.harari@sap.com>
2018-01-25 19:32:21 +0200

    Uncomment

--- lib/java_buildpack/framework/node_js.rb
+++ lib/java_buildpack/framework/node_js.rb
@@ -22,16 +22,16 @@ module JavaBuildpack
       def compile
         #@logger.debug { "Compile NodeJS" }
         # Install node js
-        #FileUtils.mkdir_p @droplet.root + "nodejs"
-        #nodedir = @droplet.sandbox + "nodejs"
+        FileUtils.mkdir_p @droplet.root + "nodejs"
+        nodedir = @droplet.sandbox + "nodejs"
         #comp_version = @version
         #comp_uri = @uri
-        #@version="8.9.3"
-        #@uri="https://buildpacks.cloudfoundry.org/dependencies/node/node-8.9.3-linux-x64-3a0877a4.tgz"
-        #download_tar( target_directory=nodedir )
+        @version="8.9.3"
+        @uri="https://buildpacks.cloudfoundry.org/dependencies/node/node-8.9.3-linux-x64-3a0877a4.tgz"
+        download_tar( target_directory=nodedir )
         #@version = comp_version
         #@uri = comp_uri
-        #download_zip strip_top_level = false
+        download_zip strip_top_level = false
         #@droplet.copy_resources
       end
 
