0ab3b459016fd61d3d04cadb16ac416f325b0c6f
Shahar Harari <shahar.harari@sap.com>
2018-01-26 14:40:51 +0200

    NodeJS binaries are not in path - fix 10

--- lib/java_buildpack/framework/node_js.rb
+++ lib/java_buildpack/framework/node_js.rb
@@ -30,10 +30,10 @@ module JavaBuildpack
         #@logger.debug { "Release NodeJS" }
         #environment_variables = @droplet.environment_variables
         #@droplet.environment_variables.add_environment_variable 'PATH', "/app/.java-buildpack/#{@droplet.component_id}/bin:$PATH"
-        @droplet.environment_variables.add_environment_variable('PATH', "$PATH:#{@droplet.root}/#{@droplet.component_id}/bin")
+        #@droplet.environment_variables.add_environment_variable('PATH', "$PATH:#{@droplet.root}/#{@droplet.component_id}/bin")
         #@droplet.environment_variables.add_environment_variable 'PATH', "#{@droplet.component_id}/bin:$PATH"
-        #@droplet.environment_variables
-        #.add_environment_variable 'PATH', "#{qualify_path(@droplet.component_id)}/bin:$PATH"
+        @droplet.environment_variables
+        .add_environment_variable 'PATH', "#{qualify_path(@droplet.component_id)}/bin:$PATH"
       end
      
       private
