8809d064f5b2ce2dd1ddeb32cb4973494424de60
amiramw <amiram.wingarten@sap.com>
2018-02-25 10:57:20 +0200

    Upgrade eclipse.jdt.ls to 0.12.1 and move to stream protocol (#18)

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -13,7 +13,7 @@ else
     echo "Create $HOME/jdt_ws_root/$moduleWs"
     mkdir $HOME/jdt_ws_root/$moduleWs
 fi
-    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root/$moduleWs
+    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.5.0.v20180119-0753.jar -configuration ./config_linux -data $HOME/jdt_ws_root/$moduleWs
 else
-    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root
+    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.5.0.v20180119-0753.jar -configuration ./config_linux -data $HOME/jdt_ws_root
 fi
