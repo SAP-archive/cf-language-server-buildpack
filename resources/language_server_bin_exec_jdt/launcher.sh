5ea859377d03b19b7854dee8b01f37dfe41105cd
idantr <idantr@gmail.com>
2018-05-22 15:52:10 +0300

    Update launcher.sh

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -11,9 +11,9 @@ if [ -d "$HOME/jdt_ws_root/$moduleWs"  ] && [ -n "$moduleWs" ]; then
     echo "Already exists $moduleWs"
 else
     echo "Create $HOME/jdt_ws_root/$moduleWs"
-    mkdir $HOME/jdt_ws_root/$moduleWs
+    mkdir -p $HOME/jdt_ws_root/$moduleWs
 fi
-    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.5.0.v20180119-0753.jar -configuration ./config_linux -data $HOME/jdt_ws_root/$moduleWs
+    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.5.0.v20180207-1446.jar -configuration ./config_linux -data $HOME/jdt_ws_root/$moduleWs
 else
-    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.5.0.v20180119-0753.jar -configuration ./config_linux -data $HOME/jdt_ws_root
+    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.5.0.v20180207-1446.jar -configuration ./config_linux -data $HOME/jdt_ws_root
 fi
