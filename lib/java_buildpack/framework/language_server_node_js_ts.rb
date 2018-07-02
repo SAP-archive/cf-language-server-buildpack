5443b4c7383a59fee277fdc151d73b94a9c6b3ce
Christian Fuchs <christian.fuchs@sap.com>
2018-07-02 16:33:19 +0200

    JS_TS: renamed languageid NodeJS (to js_ts) as requested by AW

--- lib/java_buildpack/framework/language_server_node_js_ts.rb
+++ lib/java_buildpack/framework/language_server_node_js_ts.rb
@@ -7,13 +7,13 @@ module JavaBuildpack
   module Framework
 
     # Installs JDT based LSP server component.
-    class LanguageServerNodeNODEJS < LanguageServerBase
+    class LanguageServerNodeJS_TS < LanguageServerBase
 
       # Creates an instance
       #
       # @param [Hash] context a collection of utilities used the component
       def initialize(context)
-        super(context, "NODEJS")
+        super(context, "JS_TS")
       end
 
     end
--- lib/java_buildpack/framework/node_js.rb
+++ lib/java_buildpack/framework/node_js.rb
@@ -32,7 +32,7 @@ module JavaBuildpack
 
       def enabled?
         @application.environment.key?(LSPSERVERS) &&
-        (@application.environment[LSPSERVERS].split(',') & ["cdx", "eslint", "json", "nodejs", "xml"]).any?
+        (@application.environment[LSPSERVERS].split(',') & ["cdx", "eslint", "json", "js_ts", "xml"]).any?
       end
 
       LSPSERVERS = 'lspservers'.freeze
