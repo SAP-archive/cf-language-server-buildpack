af584c03d6694439487758ea70be3f3de6ad2506
Christian Fuchs <christian.fuchs@sap.com>
2018-08-20 16:19:57 +0200

    npm install (recursive) before startup

--- resources/language_server_node_jsts/launcher.sh
+++ resources/language_server_node_jsts/launcher.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-echo 'Starting server.js using node...' > launcher.log
+
+echo 'Installing dependencies ...' > ~/tmp/ls_jsts_launcher.log
+for file in $(find $1 -name 'package.json'); do 
+    if [[ $file != *"node_modules"* ]]; 
+        then
+            echo "==== Installing dependencies for " $file
+            npm install $(dirname "$file")
+    fi
+done
+
+echo 'Starting server.js using node...' >> ~/tmp/ls_jsts_launcher.log
 exec ../node_js/bin/node ./main.js
