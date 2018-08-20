fdf141862977a7bb65063e3f5624dc463215e5fc
Christian Fuchs <christian.fuchs@sap.com>
2018-08-20 16:48:26 +0200

    output to logfile

--- resources/language_server_node_jsts/launcher.sh
+++ resources/language_server_node_jsts/launcher.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo 'Installing dependencies ...' > ~/tmp/ls_jsts_launcher.log
+echo 'Installing dependencies ...'                          > ~/tmp/ls_jsts_launcher.log
 for file in $(find $1 -name 'package.json'); do 
     if [[ $file != *"node_modules"* ]]; 
         then
-            echo "==== Installing dependencies for " $file
-            npm install $(dirname "$file")
+            echo "==== Installing dependencies for " $file  >> ~/tmp/ls_jsts_launcher.log
+            npm install $(dirname "$file")                  >> ~/tmp/ls_jsts_launcher.log
     fi
 done
 
-echo 'Starting server.js using node...' >> ~/tmp/ls_jsts_launcher.log
+echo 'Starting server.js using node...'                     >> ~/tmp/ls_jsts_launcher.log
 exec ../node_js/bin/node ./main.js
