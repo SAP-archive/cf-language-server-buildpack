93fd5b0782d6bab13d3bc249d796b8fd6ef91ba4
Christian Fuchs <christian.fuchs@sap.com>
2018-08-22 16:49:12 +0200

    JSTS-LSP: add execution permission for dependency install script

--- resources/language_server_node_jsts/launcher.sh
+++ resources/language_server_node_jsts/launcher.sh
@@ -4,6 +4,7 @@ CWD=$(pwd)
 logfile=$CWD/ls_jsts_launcher.log
 
 echo "**** Launching JSTS Language Server..."    >$logfile
+chmod 777 ./installDependencies.sh
 ./installDependencies.sh                        >>$logfile 2>&1
 echo '**** Starting server.js using node...'    >>$logfile
 ../node_js/bin/node ./main.js
