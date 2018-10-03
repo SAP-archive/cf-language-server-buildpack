071eeb173463943be5be189d1a6fb5fe57814eee
shahar-h <shahar.harari@sap.com>
2018-10-03 11:18:33 +0300

    Update launcher.sh

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -19,7 +19,7 @@ fi
     -Declipse.product=org.eclipse.jdt.ls.core.product \
     -Duser.home=$HOME \
     -noverify \
-    -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication \
+    -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication \
     -javaagent:./lombok.jar -Xbootclasspath/a:./lombok.jar \
     -jar ./plugins/org.eclipse.equinox.launcher_1.5.0.v20180512-1130.jar \
     -configuration ./config_linux \
@@ -32,7 +32,7 @@ else
     -Duser.home=$HOME \
     -noverify \
     -javaagent:./lombok.jar -Xbootclasspath/a:./lombok.jar \
-    -Xmx350M -XX:+UseG1GC -XX:+UseStringDeduplication \
+    -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication \
     -jar ./plugins/org.eclipse.equinox.launcher_1.5.0.v20180512-1130.jar \
     -configuration ./config_linux \
     -data $HOME/jdt_ws_root
