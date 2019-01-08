65fa8a29e8a2ef8c7314b88d5e2704bc25505e94
Amiram Wingarten <amiram.wingarten@sap.com>
2019-01-08 23:23:57 +0200

    uuid

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -2,22 +2,33 @@
 
 if [[ -n "$2" ]]; then
     if [[ -n "$3" ]]; then
-        moduleWs="$2/$3"
+        MODULE_WS="$2/$3"
     else
-        moduleWs="$2"
+        MODULE_WS="$2"
     fi
 else
-    moduleWs="___fallback"
+    # shouldn't happen as websocket is opened with some project path (mta or flat)
+    MODULE_WS="___fallback"
 fi
 
-wsPath="$HOME/di_ws_root/$moduleWs/___jdt_ws_root"
+UUID_FILE_PATH="$HOME/di_ws_root/$MODULE_WS/.jdt.cache.clean"
 
-mkdir -p ${wsPath}
+if [[ -s ${UUID_FILE_PATH} ]]; then
+    UUID=`cat ${UUID_FILE_PATH}`
+else
+    # compute new uuid as this is first time or file was cleaned by client code in order to clean jdt cache
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+    echo ${UUID} > ${UUID_FILE_PATH}
+fi
+
+WS_DATA_PATH="$HOME/jdt_ws_root/$MODULE_WS/$UUID"
+
+mkdir -p ${WS_DATA_PATH}
 
 exec ${JAVA_HOME}/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 \
     -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product \
     -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -javaagent:./lombok.jar \
     -Xbootclasspath/a:./lombok.jar -Xmx250M -XX:+UseG1GC -XX:+UseStringDeduplication \
     -jar ./plugins/org.eclipse.equinox.launcher_1.5.100.v20180827-1352.jar \
-    -configuration ./config_linux -data ${wsPath}
+    -configuration ./config_linux -data ${WS_DATA_PATH}
 
