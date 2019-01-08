02dd3b292d1678b9f459b700f01c2e5f4ffae1eb
Amiram Wingarten <amiram.wingarten@sap.com>
2019-01-08 15:36:38 +0200

    jdt.ls organize script and init workspace under project

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-if [ -n "$2" ]; then
-if [ -n "$3" ]; then
-    moduleWs="$2/$3"
-else 
-    moduleWs="$2"
+if [[ -n "$2" ]]; then
+    if [[ -n "$3" ]]; then
+        moduleWs="$2/$3"
+    else
+        moduleWs="$2"
+    fi
+else
+    moduleWs="___fallback"
 fi
 
-if [ -d "$HOME/jdt_ws_root/$moduleWs"  ] && [ -n "$moduleWs" ]; then
-    echo "Already exists $moduleWs"
-else
-    echo "Create $HOME/jdt_ws_root/$moduleWs"
-    mkdir -p $HOME/jdt_ws_root/$moduleWs
-fi
-    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -javaagent:./lombok.jar -Xbootclasspath/a:./lombok.jar -Xmx250M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.5.100.v20180827-1352.jar -configuration ./config_linux -data $HOME/jdt_ws_root/$moduleWs
-else
-    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -javaagent:./lombok.jar -Xbootclasspath/a:./lombok.jar -Xmx250M -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.5.100.v20180827-1352.jar -configuration ./config_linux -data $HOME/jdt_ws_root
-fi
+wsPath="$HOME/app/di_ws_root/$moduleWs/___jdt_ws_root"
+
+mkdir -p ${wsPath}
+
+exec ${JAVA_HOME}/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 \
+    -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product \
+    -Dlog.protocol=true -Dlog.level=ALL -Duser.home=$HOME -noverify -javaagent:./lombok.jar \
+    -Xbootclasspath/a:./lombok.jar -Xmx250M -XX:+UseG1GC -XX:+UseStringDeduplication \
+    -jar ./plugins/org.eclipse.equinox.launcher_1.5.100.v20180827-1352.jar \
+    -configuration ./config_linux -data ${wsPath}
+
