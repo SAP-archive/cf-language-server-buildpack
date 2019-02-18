acd1487704c9c1f3ba2d0a3bbff2a429f06257df
shahar-h <shahar.harari@sap.com>
2019-02-18 08:35:33 +0200

    Update node_js.rb

--- lib/java_buildpack/framework/node_js.rb
+++ lib/java_buildpack/framework/node_js.rb
@@ -18,7 +18,6 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
-        print 'Compiling nodejs'
         version = "8.9.3"
         uri = "https://buildpacks.cloudfoundry.org/dependencies/node/node-8.9.3-linux-x64-3a0877a4.tgz"
         download_tar(version, uri)
@@ -26,7 +25,6 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
-        print 'Releasing nodejs'
         node_bin_path = "$PWD/#{(@droplet.sandbox + 'bin').relative_path_from(@droplet.root)}"
         @droplet.environment_variables
                 .add_environment_variable "PATH", "$PATH:#{node_bin_path}"
